f132971c6f4c0f9147ed05614499a86e6000228a
Pacome Ilboudo <philboudo@mail.mccneb.edu>
2023-12-17 21:56:56 -0600

    Second method to testHolidayBonus

--- src/test/java/EmployeeObjects/SalaryEmployeeTest.java
+++ src/test/java/EmployeeObjects/SalaryEmployeeTest.java
@@ -13,6 +13,12 @@ public class SalaryEmployeeTest {
         assertEquals(1237.02, weeklyPay, 0.01);
     }
 
+    @Test
+    public void testHolidayBonus() {
+        SalaryEmployee emp = new SalaryEmployee("Steve", "Rodgers", 3781, "Sales", "Manager", 64325.0);
 
+        double bonus = emp.holidayBonus();
+        assertEquals(2164.54, bonus, 0.01);
+    }
 
 }
