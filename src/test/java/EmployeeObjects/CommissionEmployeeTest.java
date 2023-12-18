b658a3232062f93a82355ebc11da273ece42705a
Pacome Ilboudo <philboudo@mail.mccneb.edu>
2023-12-17 21:54:24 -0600

    Rest of the methods

---
+++ src/test/java/EmployeeObjects/CommissionEmployeeTest.java
@@ -0,0 +1,43 @@
+package EmployeeObjects;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CommissionEmployeeTest {
+    @Test
+    public void testIncreaseSalesPositive() {
+        CommissionEmployee emp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", .0265);
+
+        emp.increaseSales(1000);
+        assertEquals(1000.0, emp.getSales(), 0.01);
+    }
+
+    @Test
+    public void testIncreaseSalesNegative() {
+        CommissionEmployee emp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", 0.0265);
+
+        emp.increaseSales(-500); // Adding negative sales (should not change)
+        assertEquals(0.0, emp.getSales(), 0.01);
+    }
+
+    @Test
+    public void testHolidayBonus() {
+        CommissionEmployee emp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", 0.0265);
+
+        double bonus = emp.holidayBonus();
+        assertEquals(0.0, bonus, 0.01);
+    }
+
+    @Test
+    public void testAnnualRaise() {
+        CommissionEmployee emp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", 0.0265);
+
+        emp.annualRaise(); // annual raise
+        emp.annualRaise(); // Applying annual raise again
+
+        assertEquals(0.0305, emp.getRate(), 0.0001);
+    }
+
+}
--- src/test/java/EmployeeObjects/HourlyEmployeeTest.java
+++ src/test/java/EmployeeObjects/HourlyEmployeeTest.java
@@ -21,5 +21,30 @@ public class HourlyEmployeeTest {
         assertEquals(0.0, emp.getHoursWorked(), 0.01);
     }
 
+    @Test
+    public void testAnnualRaise() {
+        HourlyEmployee emp = new HourlyEmployee("Tony", "Stark", 5749, "Service", "Lead Service Manager", 32.85);
+
+        emp.annualRaise(); // Applying annual raise
+        assertEquals(34.49, emp.getWage(), 0.01);
+    }
+
+    @Test
+    public void testCalculateWeeklyPayNormalHours() {
+        HourlyEmployee emp = new HourlyEmployee("Tony", "Stark", 5749, "Service", "Lead Service Manager", 32.85);
+
+        emp.increaseHours(35); // Adding 35 hours (no overtime)
+        double weeklyPay = emp.calculateWeeklyPay();
+        assertEquals(1149.75, weeklyPay, 0.01);
+    }
+    @Test
+    public void testCalculateWeeklyPayWithOvertime() {
+        HourlyEmployee emp = new HourlyEmployee("Tony", "Stark", 5749, "Service", "Lead Service Manager", 32.85);
+
+        emp.increaseHours(45); // Adding 45 hours (with overtime)
+        double weeklyPay = emp.calculateWeeklyPay();
+        assertEquals(1560.38, weeklyPay, 0.01);
+    }
+
 
 }
